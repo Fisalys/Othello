3e8e9b40b1ce766d680785615d329992772b5e3f
Julien Stillatus <Julien.Stillatus@etu.uphfr.fr>
2021-10-20 08:26:59 +0200

    Test interface graphique

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,101 +1,120 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.geometry.NodeOrientation;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.awt.*;
+import java.io.FileInputStream;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Othello");
-        
-        //Creation de la barre de menu;
-        MenuBar bar = new MenuBar();
-
-        //Creation des menus
-        Menu partie = new Menu("Partie");
-        Menu affichage = new Menu("Affichage");
-
-        //Les Items du menu Partie
-        MenuItem newPartie = new MenuItem("Nouvelle Partie");
-        MenuItem save = new MenuItem("Sauvegarder");
-        MenuItem rejouer = new MenuItem("Revoir la partie");
-
-        //Les items du menus Affichage
-        MenuItem fullscreen = new CheckMenuItem("Plein écran");
-
-        partie.getItems().addAll(newPartie, save, rejouer);
-        affichage.getItems().addAll(fullscreen);
-
-        bar.getMenus().addAll(partie, affichage);
+            primaryStage.setTitle("Othello");
+            BorderPane root = new BorderPane();
+            root.setStyle("-fx-background-color: #d6a2a2");
+            GraphicsDevice Gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+            int height = Gd.getDisplayMode().getHeight();
+            Scene s2 = new Scene(root, 1600, 1000);
 
 
-        HBox hbox = new HBox();
-        Scene s2 = new Scene(hbox, 100, 100);
-        hbox.setSpacing(200);
-
-        VBox vbox2 =  new VBox();
-        VBox vbox3 =  new VBox();
-        GraphicsDevice Gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        int width = Gd.getDisplayMode().getWidth();
-
-        HBox hBox2 = new HBox();
-        VBox vBox1_2_1 = new VBox();
-        VBox vBox1_2_2 = new VBox();
-        hBox2.getChildren().addAll(vBox1_2_1, vBox1_2_2);
-        Button newP = new Button("Nouvelle partie");
-        vBox1_2_2.getChildren().addAll(newP);
-        Rectangle r = new Rectangle();
-        vBox1_2_1.getChildren().addAll(r);
-        r.setHeight(width);
-        r.setWidth(100);
-        r.setFill(Color.RED);
-
-        newP.setStyle("-fx-border: none;\n" +
-                "    -fx-background-color: rgb(231, 231, 5);\n" +
-                "    -fx-border-radius: 0;\n");
+            //Border pane Gauche
+            //Possède un Hbox et un Group
+            HBox bordGroup = new HBox();
+            Group barreGauche = new Group();
 
 
+            //Le groupe possède un rectangle pour la barre
+            Rectangle fill = new Rectangle();
+            fill.setWidth(75);
+            fill.setHeight(height-35);
+            fill.setFill(Color.web("#8a3d3d"));
 
-        Othello o = new Othello(s2);
 
-        vbox2.getChildren().add(o.getAff());
-        vbox2.setAlignment(Pos.CENTER);
+            //On ajoute le rectangle dans le groupe
+            barreGauche.getChildren().add(fill);
+            //On ajoute le group dans la box.
+            bordGroup.getChildren().addAll(barreGauche);
 
-        hbox.getChildren().addAll(hBox2, vbox2, vbox3);
-        newPartie.setOnAction(e -> {
-            o.resetPartie(s2);
-        });
-        fullscreen.setOnAction(e->{
-            primaryStage.setFullScreen((((CheckMenuItem)e.getSource()).isSelected()));
-        });
-        newP.setOnAction(e->{
-            primaryStage.close();
-        });
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setMaximized(true);
-        primaryStage.setScene(s2);
-        primaryStage.show();
+            //Ajout des images pour les menu
+            FileInputStream temp = new FileInputStream("ressouces/home.png");
+            Image temp2 = new Image(temp);
+            ImageView home = new ImageView(temp2);
+            Label homeView = new Label("");
+            homeView.setGraphic(home);
+            homeView.setTranslateX(7);
+            homeView.setTranslateY(100);
+            barreGauche.getChildren().add(homeView);
+
+
+            Accordion menu = new Accordion();
+            menu.setVisible(false);
+            menu.setTranslateY(100);
+            menu.setMinWidth(300);
+
+
+            homeView.setOnMouseEntered(e->{
+
+                    menu.setVisible(!(menu.isVisible()));
+            });
+            TitledPane one = new TitledPane();
+
+            one.setText("Test");
+
+            menu.getPanes().add(one);
+
+            bordGroup.getChildren().add(menu);
+            //On instancie le jeu et on le place au centre
+            Othello o = new Othello(s2);
+            root.setCenter(o.getAff());
+            //On place le hbox du menu a gauche
+            root.setLeft(bordGroup);
+
+            //On crée la barre de titre
+            ToolBar bordHaut = new ToolBar();
+            bordHaut.setMinHeight(50);
+            bordHaut.setStyle("-fx-background-color:#622b2b");
+            //On crée le bouton pour sortir de l'application
+            Button exit = new Button("X");
+            exit.setOnAction(e->{
+                primaryStage.close();
+            });
+            //On ajoute notre bouton à la barre de titre
+            bordHaut.getItems().add(exit);
+            //Permet que le bouton se retrouve à droite.
+            bordHaut.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+            root.setTop(bordHaut);
+
+            //On ajoute l'icone
+            FileInputStream input = new FileInputStream("ressouces/icon.png");
+            Image image = new Image(input);
+            ImageView icon = new ImageView(image);
+            Label iconView = new Label("");
+            iconView.setGraphic(icon);
+            iconView.setPadding(new Insets(0, 0, 0, primaryStage.getWidth()*0.94));
+            bordHaut.getItems().add(iconView);
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            primaryStage.setMaximized(true);
+
+            primaryStage.setScene(s2);
+            System.out.println(primaryStage.getScene());
+            primaryStage.show();
     }
 
 
